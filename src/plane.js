a5392c30de99db8e2190d041451c9c963d1bd549
Damien Seguin <dmnsgn@gmail.com>
2022-04-06 13:31:06 +0100

    feat: add plane direction + refactor computePlane for use in both plane and cube

--- src/plane.js
+++ src/plane.js
@@ -2,7 +2,11 @@
  * @module plane
  */
 
-import { checkArguments, getCellsTypedArray } from "./utils.js";
+import { checkArguments, computePlane, getCellsTypedArray } from "./utils.js";
+
+/**
+ * @typedef {"x" | "-x" | "y" | "-y" | "z" | "-z"} PlaneDirection
+ */
 
 /**
  * @typedef {Object} PlaneOptions
@@ -10,6 +14,7 @@ import { checkArguments, getCellsTypedArray } from "./utils.js";
  * @property {number} [sy=sx]
  * @property {number} [nx=1]
  * @property {number} [ny=nx]
+ * @property {string} [direction="z"]
  */
 
 /**
@@ -17,61 +22,25 @@ import { checkArguments, getCellsTypedArray } from "./utils.js";
  * @param {PlaneOptions} [options={}]
  * @returns {import("../types.js").SimplicialComplex}
  */
-function plane({ sx = 1, sy = sx, nx = 1, ny = nx } = {}) {
+function plane({ sx = 1, sy = sx, nx = 1, ny = nx, direction = "z" } = {}) {
   checkArguments(arguments);
 
   const size = (nx + 1) * (ny + 1);
 
-  const positions = new Float32Array(size * 3);
-  const normals = new Float32Array(size * 3);
-  const uvs = new Float32Array(size * 2);
-  const cells = new (getCellsTypedArray(size))(nx * ny * 6);
-
-  let vertexIndex = 0;
-
-  const halfSX = sx * 0.5;
-  const halfSY = sy * 0.5;
-
-  for (let y = 0; y <= ny; y++) {
-    for (let x = 0; x <= nx; x++) {
-      const u = x / nx;
-      const v = y / ny;
-
-      positions[vertexIndex * 3] = -halfSX + u * sx;
-      positions[vertexIndex * 3 + 1] = halfSY - v * sy;
-      positions[vertexIndex * 3 + 2] = 0;
-
-      normals[vertexIndex * 3] = 0;
-      normals[vertexIndex * 3 + 1] = 0;
-      normals[vertexIndex * 3 + 2] = 1;
-
-      uvs[vertexIndex * 2] = u;
-      uvs[vertexIndex * 2 + 1] = 1 - v;
-
-      if (y < ny && x < nx) {
-        const a = y * (nx + 1) + x;
-        const b = (y + 1) * (nx + 1) + x + 1;
-        const c = y * (nx + 1) + x + 1;
-        const d = (y + 1) * (nx + 1) + x;
-        cells[vertexIndex * 6] = a;
-        cells[vertexIndex * 6 + 1] = b;
-        cells[vertexIndex * 6 + 2] = c;
-
-        cells[vertexIndex * 6 + 3] = b;
-        cells[vertexIndex * 6 + 4] = a;
-        cells[vertexIndex * 6 + 5] = d;
-      }
-
-      vertexIndex++;
-    }
-  }
-
-  return {
-    positions,
-    normals,
-    uvs,
-    cells,
-  };
+  return computePlane(
+    {
+      positions: new Float32Array(size * 3),
+      normals: new Float32Array(size * 3),
+      uvs: new Float32Array(size * 2),
+      cells: new (getCellsTypedArray(size))(nx * ny * 6),
+    },
+    { vertex: 0, cell: 0 },
+    sx,
+    sy,
+    nx,
+    ny,
+    direction
+  );
 }
 
 export default plane;
--- src/utils.js
+++ src/utils.js
@@ -55,3 +55,64 @@ export function setTypedArrayType(type) {
 export const getCellsTypedArray = (size) =>
   TYPED_ARRAY_TYPE ||
   (size <= 255 ? Uint8Array : size <= 65535 ? Uint16Array : Uint32Array);
+/**
+ * @private
+ */
+const PLANE_DIRECTIONS = {
+  z: [0, 1, 2, 1, -1],
+  "-z": [0, 1, 2, -1, -1],
+  "-x": [2, 1, 0, 1, -1],
+  x: [2, 1, 0, -1, -1],
+  y: [0, 2, 1, 1, 1],
+  "-y": [0, 2, 1, 1, -1],
+};
+
+/**
+ * @private
+ */
+export function computePlane(
+  geometry,
+  indices,
+  su,
+  sv,
+  nu,
+  nv,
+  direction = "z",
+  pw = 0
+) {
+  const { positions, normals, uvs, cells } = geometry;
+  const [u, v, w, flipU, flipV] = PLANE_DIRECTIONS[direction];
+
+  const vertexOffset = indices.vertex;
+
+  for (let j = 0; j <= nv; j++) {
+    for (let i = 0; i <= nu; i++) {
+      positions[indices.vertex * 3 + u] = (-su / 2 + (i * su) / nu) * flipU;
+      positions[indices.vertex * 3 + v] = (-sv / 2 + (j * sv) / nv) * flipV;
+      positions[indices.vertex * 3 + w] = pw;
+
+      normals[indices.vertex * 3 + u] = 0;
+      normals[indices.vertex * 3 + v] = 0;
+      normals[indices.vertex * 3 + w] = pw / Math.abs(pw);
+
+      uvs[indices.vertex * 2] = i / nu;
+      uvs[indices.vertex * 2 + 1] = 1 - j / nv;
+
+      indices.vertex++;
+
+      if (j < nv && i < nu) {
+        const n = vertexOffset + j * (nu + 1) + i;
+        cells[indices.cell] = n;
+        cells[indices.cell + 1] = n + nu + 1;
+        cells[indices.cell + 2] = n + nu + 2;
+
+        cells[indices.cell + 3] = n;
+        cells[indices.cell + 4] = n + nu + 2;
+        cells[indices.cell + 5] = n + 1;
+        indices.cell += 6;
+      }
+    }
+  }
+
+  return geometry;
+}
